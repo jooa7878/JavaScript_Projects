192a5bb26558a965a892f5f01f9141d80120f5be
aiden <jooa7878@naver.com>
2021-08-02 17:34:08 +0900

    starbucks web page section 1

--- starbucks/js/script.js
+++ starbucks/js/script.js
@@ -1,6 +1,8 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 const iconEl = searchEl.querySelector('.material-icons');
+const badgeEl = document.querySelector('header .badges');
+const fadeEls = document.querySelectorAll('.visual .fade-in');
 
 searchEl.addEventListener('click', () => {
   searchInputEl.focus();
@@ -11,7 +13,36 @@ searchInputEl.addEventListener('focus', () => {
   searchInputEl.setAttribute('placeholder', '통합검색');
 });
 
-searchInputEl.addEventListener('blur', ()=>{
+searchInputEl.addEventListener('blur', () => {
   searchEl.classList.remove('focused');
   searchInputEl.setAttribute('placeholder', '');
+})
+
+window.addEventListener('scroll', _.throttle(() => {
+  console.log('scroll');
+  if (window.scrollY > 500) {
+    // hide badge
+
+    //gsap.to(요소, 지속시간, 옵션);
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    })
+
+  } else {
+    // display badge
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    })
+  }
+}, 300));
+
+// -.throttle(function, time);
+
+fadeEls.forEach( (fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1
+  });
 })
