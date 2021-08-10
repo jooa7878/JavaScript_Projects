1e9514ae12b76914887f5b75f1b018ebcc1f2805
aiden <jooa7878@naver.com>
2021-08-10 17:20:24 +0900

    added slide & toggle

--- starbucks/js/script.js
+++ starbucks/js/script.js
@@ -3,6 +3,9 @@ const searchInputEl = searchEl.querySelector('input');
 const iconEl = searchEl.querySelector('.material-icons');
 const badgeEl = document.querySelector('header .badges');
 const fadeEls = document.querySelectorAll('.visual .fade-in');
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
 
 searchEl.addEventListener('click', () => {
   searchInputEl.focus();
@@ -46,3 +49,42 @@ fadeEls.forEach( (fadeEl, index) => {
     opacity: 1
   });
 })
+
+
+// SWIPER
+
+new Swiper('.notice-line .swiper-container', {
+  direction : 'vertical',
+  autoplay : true,
+  loop : true,
+  clickable : true
+}); // 생성자 (클래스)
+
+
+new Swiper('.notice .promotion .swiper-container', {
+  slidesPerView : 3, // 한번에 보여줄 슬라이드 개수
+  spaceBetween : 10,
+  centeredSlides : true,
+  loop : true,
+  autoplay : {
+    delay : 5000
+  },
+  pagination : {
+    el : '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable : true,
+  },
+  navigation : {
+    prevEl : '.promotion .swiper-prev',
+    nextEl : '.promotion .swiper-next'
+  }
+});
+
+promotionToggleBtn.addEventListener('click', ()=>{
+  isHidePromotion = !isHidePromotion;
+
+  if(isHidePromotion){ // 숨김처리
+   promotionEl.classList.add('hide');
+  }else { // 보여주세요
+    promotionEl.classList.remove('hide');
+  }
+});
